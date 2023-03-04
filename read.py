db6214a5efd468008f181d1cd540fe31a7f0e4c0
Walter-hsieh <tlhsieh0207@gmail.com>
2023-03-04 15:42:49 +0800

    2nd

--- read.py
+++ read.py
@@ -1,8 +1,32 @@
-# with open("a_1_sec.txt", 'r') as f:
-# 	for line in f:
-# 		print(line)
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-with open("mof-801_1_sec.txt", 'r') as f:
-	last_line = f.readlines()[-4].split()[1]
-	print(last_line, type(last_line))
+
+# oepn txt file and read lines
+
+def read_txt():
+	with open("test_dataset.txt", 'r') as f:
+		last_line = f.readlines()[-4].split()[1]
+		print(last_line, type(last_line))
+
+
+data = {'time':[], 'gram':[]}
+
+
+# make into dataframe with pandas
+with open("test_dataset.txt", 'r') as file:
+	lines = file.readlines()
+
+	for line in lines[16:len(lines)-4]:
+		time, gram = line.split()
+		data['time'].append(time)
+		data['gram'].append(float(gram))
+
+df = pd.DataFrame(data)
+
+print(df.head())
+
+plt.plot(df['gram'])
+plt.show()
+
